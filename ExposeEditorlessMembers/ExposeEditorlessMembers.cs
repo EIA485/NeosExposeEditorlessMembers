d9e4ed48826133e752655fe3f9cdb6cc2e9b3ed5
ftfft <17285570+EIA485@users.noreply.github.com>
2025-08-25 06:06:02 -0700

    Port to BepisLoader/Resonite

--- ExposeEditorlessMembers/ExposeEditorlessMembers.cs
+++ ExposeEditorlessMembers/ExposeEditorlessMembers.cs
@@ -1,31 +1,46 @@
-﻿using HarmonyLib;
-using NeosModLoader;
-using System;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
 using FrooxEngine;
-using System.Collections;
-using System.Collections.Generic;
+using HarmonyLib;
+using System.Reflection;
 using System.Reflection.Emit;
 
 namespace ExposeEditorlessMembers
 {
-	public class ExposeEditorlessMembers : NeosMod
+    [ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+    [BepInDependency(BepInExResoniteShim.PluginMetadata.GUID)]
+    public class ExposeEditorlessMembers : BasePlugin
 	{
-		public override string Name => "ExposeEditorlessMembers";
-		public override string Author => "eia485";
-		public override string Version => "1.0.0";
-		public override string Link => "https://github.com/EIA485/NeosExposeEditorlessMembers/";
-		public override void OnEngineInit()
+		static ManualLogSource log;
+		static ConfigEntry<bool> ShowHidden;
+		static bool HiddenPatched = false;
+		const string HiddenCat = "HiddenToggle";
+        public override void Load()
 		{
-			Harmony harmony = new Harmony("net.eia485.ExposeEditorlessMembers");
-			harmony.PatchAll();
-		}
+			ShowHidden = Config.Bind(PluginMetadata.NAME, "Ignore HideInInspectorAttribute", true);
+			log = Log;
+			HarmonyInstance.PatchAllUncategorized();
+            ShowHidden.SettingChanged += ShowHidden_SettingChanged;
+			ShowHidden_SettingChanged();
+        }
 
-		[HarmonyPatch(typeof(SyncMemberEditorBuilder), nameof(SyncMemberEditorBuilder.Build))]
-		class ExposeEditorlessMembersPatch
+        private void ShowHidden_SettingChanged(object? sender = null, EventArgs e = null)
+        {
+			if (ShowHidden.Value == HiddenPatched) return;
+			if (ShowHidden.Value) HarmonyInstance.PatchCategory(HiddenCat);
+            else HarmonyInstance.UnpatchCategory(HiddenCat);
+			HiddenPatched = ShowHidden.Value;
+        }
+
+        [HarmonyPatch(typeof(SyncMemberEditorBuilder), nameof(SyncMemberEditorBuilder.Build))]
+		class SyncMemberEditorBuilderPatch
 		{
 			static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
 			{
-				List<CodeInstruction> instructions = new List<CodeInstruction>(codes);
+				List<CodeInstruction> instructions = new(codes);
 				int index = -1;
 				object label = null;
 				for (int i = 1; i < instructions.Count - 1; i++)
@@ -33,29 +48,56 @@ namespace ExposeEditorlessMembers
 					if (instructions[i].opcode == OpCodes.Isinst && instructions[i].operand == typeof(EmptySyncElement) &&
 						instructions[i - 1].opcode == OpCodes.Ldarg_0 && instructions[i + 1].opcode == OpCodes.Stloc_S)
 					{
-
 						label = instructions[i + 1].operand;
 						index = i - 1;
 						break;
 					}
 
 				}
-				if(index != -1)
-				{
-					instructions.RemoveAt(index);
-					instructions.RemoveAt(index);
-					instructions.RemoveAt(index);
-				}
 				if (label != null)
 				{
-                    for (int i = 0; i < instructions.Count; i++)
-                    {
+					instructions.RemoveAt(index);
+					instructions.RemoveAt(index);
+					instructions.RemoveAt(index);
+					for (int i = 0; i < instructions.Count; i++)
+					{
 						if (instructions[i].opcode == OpCodes.Ldloc_S && instructions[i].operand == label)
 							instructions[i] = new(OpCodes.Ldarg_0);
-                    }
-                }
+					}
+				}
+				else log.Log(LogLevel.Error, "SyncMemberEditorBuilder.Build Transpiler Failed");
+
 				return instructions;
 			}
-		}
+        }
+
+		[HarmonyPatchCategory(HiddenCat)]
+        [HarmonyPatch(typeof(WorkerInspector), nameof(WorkerInspector.BuildInspectorUI))]
+        class WorkerInspectorPatch //HideInInspectorAttribute check
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes) 
+			{
+                List<CodeInstruction> instructions = new(codes);
+                int index = -1;
+                for (int i = 2; i < instructions.Count - 1; i++)
+                {
+                    if (instructions[i-2].opcode == OpCodes.Ldarg_0 && instructions[i].opcode == OpCodes.Callvirt && instructions[i + 1].opcode == OpCodes.Call && (instructions[i+1].operand as MethodInfo)?.Name == "GetCustomAttribute")
+                    {
+                        index = i - 2;
+                        break;
+                    }
+                }
+                if (index > -1)
+                {
+                    instructions.RemoveAt(index);
+                    instructions.RemoveAt(index);
+                    instructions.RemoveAt(index);
+                    instructions.RemoveAt(index);
+                    instructions.RemoveAt(index);
+                }
+                else log.Log(LogLevel.Error, "WorkerInspector.BuildInspectorUI Transpiler Failed");
+                return instructions;
+            }
+        }
 	}
 }
